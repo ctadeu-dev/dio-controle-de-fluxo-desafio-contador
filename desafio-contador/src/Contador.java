8eb8b422f3f9a0ca9d6bdaca5e20d2614518a6c2
ctadeu-dev <xtadeu@outlook.com>
2025-06-02 19:07:37 -0300

    Criando projeto

---
+++ desafio-contador/src/Contador.java
@@ -0,0 +1,13 @@
+public class Contador {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+
+        int contagem = parametroDois - parametroUm;
+        //realizar o for para imprimir os números com base na variável contagem
+    }
+
+}
